10342afa1da3f25a284a55146b9aa6483b18594c
aiocat <aiocat@protonmail.com>
2021-10-08 21:34:41 +0300

    added find text utility.

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,11 @@ fn main() {
     app.start();
 }
 
-fn main_handler(found_flags: &Vec<surd::Flag>, _: Vec<String>) {
+fn main_handler(found_flags: &Vec<surd::Flag>, program_args: Vec<String>) {    
+    if program_args.len() == 0 {
+        return
+    }
+    
     let mut program_path = &String::from(".");
 
     for flag in found_flags {
@@ -19,18 +23,15 @@ fn main_handler(found_flags: &Vec<surd::Flag>, _: Vec<String>) {
             program_path = &flag.value
         }
     }
-    
-    find_text(program_path);
+    handle_path(program_path, program_args);
 }
 
-fn find_text(program_path: &String) {
+fn handle_path(program_path: &String, program_args: Vec<String>) {
     match fs::read_dir(program_path) {
         Ok(rd) => {
             for path in rd {
                 match path {
-                    Ok(file) => {
-                        println!("{:?}", file.file_name())
-                    },
+                    Ok(file) => wrap_file(file, program_args.clone()),
                     Err(err) => {
                         let err_vec: Vec<Styles> = vec![Styles::Bold, Styles::FgColor256(9)];
                         r256::println(&err_vec, &err.to_string());
@@ -38,10 +39,60 @@ fn find_text(program_path: &String) {
                 }
             }
         },
-
         Err(err) => {
             let err_vec: Vec<Styles> = vec![Styles::Bold, Styles::FgColor256(9)];
             r256::println(&err_vec, &err.to_string());
         }
     }
+}
+
+fn wrap_file(file: std::fs::DirEntry, program_args: Vec<String>) {
+    match file.metadata() {
+        Ok(metadata) => {
+            let file_path = file.path().display().to_string();
+
+            if metadata.is_dir() {
+                handle_path(&file_path, program_args)
+            } else {
+                match fs::read_to_string(&file_path) {
+                    Ok(file_data) => {
+                        let printable_str = find_text(file_data, program_args.join(" "));
+
+                        if printable_str.len() != 0 {
+                            println!("{} [{}]\n{}{}\n", 
+                              r256::generate_string(&vec![Styles::Bold, Styles::FgColor256(10)], &file_path),
+                              r256::generate_string(&vec![Styles::FgColor256(15)], &metadata.len().to_string()),
+                              "-".repeat(file_path.len()),
+                              printable_str
+                            )
+                        }
+                    },
+                    Err(_) => return
+                }
+            }
+        },
+        Err(err) => {
+            let err_vec: Vec<Styles> = vec![Styles::Bold, Styles::FgColor256(9)];
+            r256::println(&err_vec, &err.to_string());
+        }
+    }
+}
+
+fn find_text(data: String, find: String) -> String {
+    let mut formatted_text = String::new();
+
+    for (index, line) in data.lines().enumerate() {
+        if line.contains(&find) {
+            let colored_found_text = r256::generate_string(&vec![Styles::BgColor256(4), Styles::FgColor256(15)], &find);
+            let got_str = line.replace(&find, &colored_found_text);
+
+            if line == got_str {
+                continue
+            }
+
+            formatted_text = format!("{}\n{}:{}", formatted_text, r256::generate_string(&vec![Styles::Bold, Styles::FgColor256(11)], &index.to_string()), got_str)
+        }
+    }
+
+    formatted_text
 }
